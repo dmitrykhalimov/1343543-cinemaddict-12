a4e4aceab713e428eefc7c59bbe1a1a3b643cd57
Bluesman <dmitry.khalimov@gmail.com>
2020-08-17 12:34:56 +0300

    Исправляет блок extras

--- src/mock/extras.js
+++ src/mock/extras.js
@@ -1,23 +1,7 @@
 export const generateTopRated = (films) => {
-  return films.concat().sort(function (a, b) {
-    if (a.rating > b.rating) {
-      return -1;
-    }
-    if (a.rating < b.rating) {
-      return 1;
-    }
-    return 0;
-  });
+  return films.slice().sort((a, b) => b.rating - a.rating);
 };
 
 export const generateTopCommented = (films) => {
-  return films.concat().sort(function (a, b) {
-    if (a.comments.length > b.comments.length) {
-      return -1;
-    }
-    if (a.comments.length < b.comments.length) {
-      return 1;
-    }
-    return 0;
-  });
+  return films.slice().sort((a, b) => b.comments.length - a.comments.length);
 };
