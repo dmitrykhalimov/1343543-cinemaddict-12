006d7cbec0e27ca3a801b9290dabe286c29c9915
Bluesman <dmitry.khalimov@gmail.com>
2020-08-19 10:58:11 +0300

    Осуществляет рефакторинг простых функций

--- src/view/abstract.js
+++ src/view/abstract.js
@@ -1,7 +1,33 @@
+import {createElement} from "../utils.js";
+
 export default class Abstract {
   constructor() {
     if (new.target === Abstract) {
-      throw new Error(`You're trying to instntiate an Abstract class, Mario. Princess in the concrete one.`);
+      throw new Error(`You're trying to instantiate an Abstract class, Mario. Princess in the concrete one.`);
     }
+
+    this._element = null;
+  }
+
+  getTemplate() {
+    throw new Error(`Abstract method is not implemented: getTemplate`);
+  }
+
+  getElement() {
+    /* if (new.target === Abstract) {
+      throw new Error(`You're trying to instantiate an Abstract class, Mario. Princess in the concrete one.`);
+    } */
+
+    if (!this._element) {
+      this._element = createElement(this.getTemplate());
+    }
+
+    return this._element;
+  }
+
+  removeElement() {
+    this._element = null;
   }
 }
+
+
--- src/view/button.js
+++ src/view/button.js
@@ -1,27 +1,11 @@
-import {createElement} from "../utils.js";
+import AbstractView from "./abstract.js";
 
 const createButton = () => {
   return `<button class="films-list__show-more">Show more</button>`;
 };
 
-export default class Button {
-  constructor() {
-    this._element = null;
-  }
-
+export default class Button extends AbstractView {
   getTemplate() {
     return createButton();
-  }
-
-  getElement() {
-    if (!this._element) {
-      this._element = createElement(this.getTemplate());
-    }
-
-    return this._element;
-  }
-
-  removeElement() {
-    this._element = null;
   }
 }
