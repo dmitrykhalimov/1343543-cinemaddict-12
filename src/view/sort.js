6b4579f23720bcc4d645d9723ef5dd0de371817e
Bluesman <dmitry.khalimov@gmail.com>
2020-08-20 12:47:24 +0300

    Добавляет файл констант, меняет разметку блока сортировки

--- src/view/sort.js
+++ src/view/sort.js
@@ -1,10 +1,11 @@
 import AbstractView from "./abstract.js";
+import {SortType} from "../const.js";
 
 const createSort = () => {
   return `<ul class="sort">
-      <li><a href="#" class="sort__button sort__button--active">Sort by default</a></li>
-      <li><a href="#" class="sort__button">Sort by date</a></li>
-      <li><a href="#" class="sort__button">Sort by rating</a></li>
+      <li><a href="#" data-sort-type="${SortType.DEFAULT}" class="sort__button sort__button--active">Sort by default</a></li>
+      <li><a href="#" data-sort-type="${SortType.DATE}" class="sort__button">Sort by date</a></li>
+      <li><a href="#" data-sort-type="${SortType.RATING}" class="sort__button">Sort by rating</a></li>
     </ul>`;
 };
 
