f706ec3916255603900f380435f7368ea8c8e254
Bluesman <dmitry.khalimov@gmail.com>
2020-09-15 11:29:12 +0300

    Исправляет undefined в описании  если с сервера не приходят жанры

--- src/view/film-card.js
+++ src/view/film-card.js
@@ -17,13 +17,14 @@ export const createFilmCard = (task) => {
     return descriptionToShorten.slice(0, MAX_DESCRIPTION_LENGTH - 1) + `...`;
   };
 
+  console.log(genres.size);
   return `<article class="film-card">
     <h3 class="film-card__title">${title}</h3>
     <p class="film-card__rating">${rating}</p>
     <p class="film-card__info">
       <span class="film-card__year">${transformDateTime(filmDate, DateFormats.ONLY_YEAR)}</span>
       <span class="film-card__duration">${translateMinutesToText(duration)}</span>
-      <span class="film-card__genre">${genres.values().next().value}</span>
+      <span class="film-card__genre">${genres.size > 0 ? genres.values().next().value : ``}</span>
     </p>
     <img src="./${poster}" alt="" class="film-card__poster">
     <p class="film-card__description">${description.length <= MAX_DESCRIPTION_LENGTH ? description : shortenDescription(description)}</p>
